0a5ba44210c45be34217dee681c7e7aba34058f9
Fiona <fionatqh@yahoo.com>
2020-09-21 00:37:13 +0800

    added shelf class

--- src/test/java/com/horcrux/hufflepuff/ShelfTester.java
+++ src/test/java/com/horcrux/hufflepuff/ShelfTester.java
@@ -1,40 +1,40 @@
 package com.horcrux.hufflepuff;
-//import org.junit.Assert;
-//import org.junit.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class ShelfTester {
-//
-//    @Test
-//    public void shelfCanAcceptAndReturnItem() {
-//        Shelf shelf = new Shelf();
-//        shelf.put("Orange");
-//        Assert.assertTrue(shelf.take("Orange"));
-//    }
-//
-//    @Test
-//    public void shelfCannotAcceptEmptyOrNull() {
-//        Shelf shelf = new Shelf();
-//        shelf.put("");
-//        Assert.assertFalse(shelf.take(""));
-//        shelf.put(null);
-//        Assert.assertFalse(shelf.take(null));
-//    }
-//
-//    @Test
-//    public void shelfCannotTakeMoreThanOnce() {
-//        Shelf shelf = new Shelf();
-//        shelf.put("Orange");
-//        Assert.assertTrue(shelf.take("Orange"));
-//        Assert.assertFalse(shelf.take("Orange"));
-//    }
-//
-//    @Test
-//    public void shelfCanHaveDuplicateItems() {
-//        Shelf shelf = new Shelf();
-//        shelf.put("Orange");
-//        shelf.put("Orange");
-//        Assert.assertTrue(shelf.take("Orange"));
-//        Assert.assertTrue(shelf.take("Orange"));
-//    }
-//
+
+    @Test
+    public void shelfCanAcceptAndReturnItem() {
+        Shelf shelf = new Shelf();
+        shelf.put("Orange");
+        Assert.assertTrue(shelf.take("Orange"));
+    }
+
+    @Test
+    public void shelfCannotAcceptEmptyOrNull() {
+        Shelf shelf = new Shelf();
+        shelf.put("");
+        Assert.assertFalse(shelf.take(""));
+        shelf.put(null);
+        Assert.assertFalse(shelf.take(null));
+    }
+
+    @Test
+    public void shelfCannotTakeMoreThanOnce() {
+        Shelf shelf = new Shelf();
+        shelf.put("Orange");
+        Assert.assertTrue(shelf.take("Orange"));
+        Assert.assertFalse(shelf.take("Orange"));
+    }
+
+    @Test
+    public void shelfCanHaveDuplicateItems() {
+        Shelf shelf = new Shelf();
+        shelf.put("Orange");
+        shelf.put("Orange");
+        Assert.assertTrue(shelf.take("Orange"));
+        Assert.assertTrue(shelf.take("Orange"));
+    }
+
 }
